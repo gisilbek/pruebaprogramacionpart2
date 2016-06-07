24f874a6516df02d4b2e25e8dd604aa5bfdf7b79
Gisilbek <gisilbek@gmail.com>
2016-06-06 23:43:00 -0430

    Base Datos
    Base de Datos y Query
    Seccion Modelo de Datos

---
+++ BaseDatos/Query.sql
@@ -0,0 +1,23 @@
+﻿select B.documentsid, B.namefirst,B.namelast, C.name
+from admcurso.inscripcion as A, admcurso.alumnos as B, admcurso.curso as C
+where A.idcurso =2 and C.id= 2 and A.idalumnos = B.id
+
+SELECT AVG(nota)as Promedio 
+FROM admcurso.nota as A
+where  A.idalumnos = 2 
+
+
+SELECT AVG(nota)as Promedio 
+FROM admcurso.nota as A
+where  A.idalumnos = 2 and A.idprofesor=1
+
+
+select B.documentsid, B.namefirst,B.namelast, C.name,AVG(A.nota)as Promedio  
+from admcurso.nota as A, admcurso.alumnos as B, admcurso.curso as C
+where A.idcurso =2 and C.id= 2 and A.idalumnos = B.id  and (select count (nota) as cantidad from admcurso.nota where idcurso =2  and idalumnos = 2 and nota<10 ) >=2
+group by B.documentsid, B.namefirst,B.namelast, C.name, A.nota 
+
+/*
+La respuesta la pregunta 6 de la seccion Manejo de Datos es la C
+
+*/
